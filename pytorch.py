8b5d5fd1bc954468f3ad862e701880c502386401
Chami Lamelas <chami.lamelas@gmail.com>
2023-09-30 17:38:03 -0400

    Update pytorch.py

--- pytorch.py
+++ pytorch.py
@@ -6,10 +6,14 @@ import torch
 import sys
 
 DESCRIPTION = f"""
-Python version: {sys.version}
-PyTorch version: {torch.__version__}
-Number of CUDA devices seen by PyTorch: {torch.cuda.device_count()}
-Whether CUDA is available in PyTorch installation: {torch.cuda.is_available()}
+Python version:
+\t{sys.version}
+PyTorch version:
+\t{torch.__version__}
+Number of CUDA devices seen by PyTorch:
+\t{torch.cuda.device_count()}
+Whether CUDA is available in PyTorch installation:
+\t{torch.cuda.is_available()}
 """
 
 print(DESCRIPTION)
