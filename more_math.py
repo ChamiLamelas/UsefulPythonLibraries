ff1095d5c40f8513b79731ca461286911a226cf8
Chami Lamelas <chami.lamelas@gmail.com>
2023-10-02 12:19:50 -0400

    Update more_math.py

--- more_math.py
+++ more_math.py
@@ -3,3 +3,6 @@ import random
 def argmax(choices, criteria, args=None):
     m = max(criteria(c, args) for c in choices)
     return random.choice(list(filter(lambda c: criteria(c, args) == m, choices)))
+
+def check_equal_shapes(*arrs):
+    return len({a.shape for a in arrs}) == 1
